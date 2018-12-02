3b1fec39d6acfbd665a65aed500e34ca975bb317
Cong Nguyen <rampadc@users.noreply.github.com>
2018-12-02 16:10:32 +1100

    Added configuration with XML

--- src/main/kotlin/guru/springframework/jokeapp/JokeAppApplication.kt
+++ src/main/kotlin/guru/springframework/jokeapp/JokeAppApplication.kt
@@ -2,8 +2,10 @@ package guru.springframework.jokeapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 class JokeAppApplication
 
 fun main(args: Array<String>) {
